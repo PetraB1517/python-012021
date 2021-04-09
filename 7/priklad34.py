7cc94db2ff4fde8862bc8873175787b0e4985c27
Petra Bartoňová <artep.Bart@seznam.cz>
2021-04-09 13:30:33 +0200

    příklad 34 (v zadání 33) hotov i s rozšířením

--- 7/priklad34.py
+++ 7/priklad34.py
@@ -1,7 +1,60 @@
-Teplota ve městech popáté
-Vrať se k práci se souborem temperature.csv, který obsahuje informace o průměrné teplotě v různých městech v listopadu 2017.
+#Velikonoce
+"""Ze souboru velikonoce.csv načti data o tom, kolikrát na který datum připadaly Velikonoce v letech 1600 až 2100.
 
 import wget
-wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/temperature.csv")
-Vytvoř tabulku, která bude obsahovat údaje o teplotě za města Helsinki, Miami Beach a Tokyo.
-Vytvoř krabicový graf a porovnej rozsah teplot v těchto městech.
+
+wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/7/velikonoce.csv")
+Vytvoř sloupcový graf, který data přehledně zobrazí.
+Na ose x budou vidět jednotlivá data ("datumy") a výška sloupce označí, kolikrát na daný den připadly Velikonoce.
+Tentokrát jsou popisky a titulek povinné :-)
+
+Po zavolání funkce plot() si výsledek ulož do proměnné ax. Následně zavolej metodu set_ylabel(),
+abys nastavila popisek osy y grafu.
+
+ax = velikonoce.plot()
+ax.set_ylabel("Počet dnů")"""
+
+
+import pandas
+import matplotlib.pyplot as plt
+
+velikonoce = pandas.read_csv('velikonoce.csv')
+velikonoce = velikonoce.set_index('Datum')
+
+plt.figure(1)
+ax = velikonoce.plot(kind='bar')
+ax.set_xlabel("Datum")
+ax.set_ylabel("Počet dnů")
+ax.set_title('Velikonoční dny')
+ax.tick_params(axis='x', labelrotation=65, labelsize='small')
+ax.get_legend().remove()
+#plt.show()
+
+
+
+
+"""Rozšířené zadání
+
+Vytvoř si datový soubor sama. Můžeš k tomu využít modul dateutil, který při instalaci najdeš pod jménem python-dateutil.
+Následně si zkopíruj kód níže a doplň na místo komentářů příkazy, které prováději požadovanou činnost."""
+
+import pandas
+from dateutil import easter
+
+data = []
+for rok in range(1900,2020):
+  datum = easter.easter(rok)
+  datum = datum.strftime('%d.%m.')
+  data.append(datum)
+
+
+data = pandas.DataFrame(data, columns=["Datum"])
+data = data.groupby("Datum").size()
+
+plt.figure(1)
+ax = data.plot(kind='bar')
+ax.set_xlabel("Datum")
+ax.set_ylabel("Počet dnů")
+ax.set_title('Velikonoční dny')
+ax.tick_params(axis='x', labelrotation=65, labelsize='small')
+plt.show()
