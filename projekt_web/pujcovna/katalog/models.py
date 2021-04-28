a968f38fe63f81ec048ed204bf1dfbb10cea7efc
Petra Bartoňová <artep.Bart@seznam.cz>
2021-04-28 17:38:49 +0200

    Všechny úkoly ze zadání 9 (41až45) #9

--- projekt_web/pujcovna/katalog/models.py
+++ projekt_web/pujcovna/katalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+  registracniZnacka = models.CharField(max_length=100)
+  znackaATyp = models.CharField(max_length=100)
+  pocetNajetychKm = models.IntegerField()
+  datumPosledniTK = models.DateField()
+
+
+class Zakaznik(models.Model):
+  krestniJmeno = models.CharField(max_length=100)
+  prijmeni = models.CharField(max_length=100)
+  cisloRidicskehoPrukazu = models.CharField(max_length=40)
+  datumNarozeni = models.DateField()
+
+class Vypujcka(models.Model):
+  datumVypujcky = models.DateTimeField()
+  datumVraceni = models.DateTimeField()
+  cena = models.IntegerField()
+
